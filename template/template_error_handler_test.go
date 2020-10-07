4aad76b069efb666fbee6578ea922ba33966e6e2
Jo Giroux <jgiroux@coveo.com>
2020-10-07 12:13:54 -0400

    Fix failing tests

--- template/template_error_handler_test.go
+++ template/template_error_handler_test.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -39,6 +40,36 @@ func TestTemplateErrorHandling(t *testing.T) {
 				assert.NoError(t, err)
 			} else {
 				assert.Equal(t, tt.errCount, strings.Count(err.Error(), tt.err))
+			}
+		})
+	}
+}
+
+func Test_templateWithErrors(t *testing.T) {
+	t.Parallel()
+
+	template, _ := NewTemplate(".", nil, "", nil)
+	template.SetOption(StrictErrorCheck, true)
+	tests := []struct {
+		name    string
+		content string
+		err     error
+	}{
+		{"Empty template", "", nil},
+		{"Non closed brace", "{{", fmt.Errorf("Non closed brace:1: unexpected unclosed action in command in: {{")},
+		{"Non opened brace", "}}", nil},
+		{"Undefined value", "@value", fmt.Errorf("Undefined value:1: contains undefined value(s) in: @value")},
+		{"2 Undefined values", "@(value1 + value2)", fmt.Errorf("2 Undefined values:1: contains undefined value(s) in: @(value1 + value2)")},
+		{"Several errors", "@(value1)\n@non_Existing_Func()\n{{\n", fmt.Errorf("Several errors:2: function \"non_Existing_Func\" not defined in: @non_Existing_Func()\nSeveral errors:3: unexpected unclosed action in command in: {{\nSeveral errors:1: contains undefined value(s) in: @(value1)")},
+		{"undefined variable", "@(value_non_existing)", fmt.Errorf("undefined variable:1: contains undefined value(s) in: @(value_non_existing)")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, err := template.ProcessContent(tt.content, tt.name); err != tt.err {
+				if err != nil && tt.err != nil && err.Error() == tt.err.Error() {
+					return
+				}
+				t.Errorf("ProcessContent()=\n%v\n\nWant:\n%v", err, tt.err)
 			}
 		})
 	}
--- template/template_test.go
+++ template/template_test.go
@@ -1,7 +1,6 @@
 package template
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
@@ -86,36 +85,6 @@ func TestTemplateFilesOverwrite(t *testing.T) {
 			result, err := ioutil.ReadFile(tempfile.Name())
 			assert.Nil(t, err)
 			assert.Equal(t, tt.result, string(result))
-		})
-	}
-}
-
-func Test_templateWithErrors(t *testing.T) {
-	t.Parallel()
-
-	template, _ := NewTemplate(".", nil, "", nil)
-	template.SetOption(StrictErrorCheck, true)
-	tests := []struct {
-		name    string
-		content string
-		err     error
-	}{
-		{"Empty template", "", nil},
-		{"Non closed brace", "{{", fmt.Errorf("Non closed brace:1: unexpected unclosed action in command in: {{")},
-		{"Non opened brace", "}}", nil},
-		{"Undefined value", "@value", fmt.Errorf("Undefined value:1:4: Undefined value value in: @value")},
-		{"2 Undefined values", "@(value1 + value2)", fmt.Errorf("2 Undefined values:1:8: Undefined value value1 in: @(value1 + value2)\n2 Undefined values:1:21: Undefined value value2 in: @(value1 + value2)")},
-		{"Several errors", "@(value1)\n@non_Existing_Func()\n{{\n", fmt.Errorf("Several errors:2: function \"non_Existing_Func\" not defined in: @non_Existing_Func()\nSeveral errors:3: unexpected unclosed action in command in: {{\nSeveral errors:1:4: Undefined value value1 in: @(value1)")},
-		{"undefined variable", "@(value_non_existing)", fmt.Errorf("undefined variable:1:4: Undefined value value_non_existing in: @(value_non_existing)")},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if _, err := template.ProcessContent(tt.content, tt.name); err != tt.err {
-				if err != nil && tt.err != nil && err.Error() == tt.err.Error() {
-					return
-				}
-				t.Errorf("ProcessContent()=\n%v\n\nWant:\n%v", err, tt.err)
-			}
 		})
 	}
 }
